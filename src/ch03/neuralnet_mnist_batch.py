ef7f8df25608c8ee3195b6d30094288b832ecd55
Shota Shimizu <s_shimizu@pkshatech.com>
2018-11-03 17:46:52 +0900

    make lint

--- src/ch03/neuralnet_mnist_batch.py
+++ src/ch03/neuralnet_mnist_batch.py
@@ -1,47 +1,57 @@
 # coding: utf-8
-import sys, os
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+import sys
+import os
 import numpy as np
 import pickle
-from dataset.mnist import load_mnist
-from common.functions import sigmoid, softmax
+from pathlib import Path
+from ..dataset.mnist import load_mnist
+from ..common.functions import sigmoid, softmax
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def get_data():
-    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
-    return x_test, t_test
+        (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True,
+                                                          flatten=True,
+                                                          one_hot_label=False)
+        return x_test, t_test
 
 
 def init_network():
-    with open("sample_weight.pkl", 'rb') as f:
-        network = pickle.load(f)
-    return network
+        # Get parent directory
+        pardir = Path(__file__).parent.resolve()
+        pickle_path = os.path.join(pardir, "sample_weight.pkl")
+        with open(pickle_path, 'rb') as f:
+                network = pickle.load(f)
+        return network
 
 
 def predict(network, x):
-    w1, w2, w3 = network['W1'], network['W2'], network['W3']
-    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+        w1, w2, w3 = network['W1'], network['W2'], network['W3']
+        b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
-    a1 = np.dot(x, w1) + b1
-    z1 = sigmoid(a1)
-    a2 = np.dot(z1, w2) + b2
-    z2 = sigmoid(a2)
-    a3 = np.dot(z2, w3) + b3
-    y = softmax(a3)
+        a1 = np.dot(x, w1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, w2) + b2
+        z2 = sigmoid(a2)
+        a3 = np.dot(z2, w3) + b3
+        y = softmax(a3)
 
-    return y
+        return y
 
 
-x, t = get_data()
-network = init_network()
+if __name__ == '__main__':
+        x, t = get_data()
+        network = init_network()
 
-batch_size = 100 # バッチの数
-accuracy_cnt = 0
+        batch_size = 100  # バッチの数
+        accuracy_cnt = 0
 
-for i in range(0, len(x), batch_size):
-    x_batch = x[i:i+batch_size]
-    y_batch = predict(network, x_batch)
-    p = np.argmax(y_batch, axis=1)
-    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+        for i in range(0, len(x), batch_size):
+                x_batch = x[i:i+batch_size]
+                y_batch = predict(network, x_batch)
+                p = np.argmax(y_batch, axis=1)
+                accuracy_cnt += np.sum(p == t[i:i+batch_size])
 
-print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+        logging.info(f"Accuracy: {str(float(accuracy_cnt) / len(x))}")
--- src/ch04/gradient_method.py
+++ src/ch04/gradient_method.py
@@ -2,35 +2,39 @@
 import numpy as np
 import matplotlib.pylab as plt
 from gradient_2d import numerical_gradient
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def gradient_descent(f, init_x, lr=0.01, step_num=100):
-    x = init_x
-    x_history = []
+        x = init_x
+        x_history = []
 
-    for i in range(step_num):
-        x_history.append( x.copy() )
+        for i in range(step_num):
+                x_history.append(x.copy())
 
-        grad = numerical_gradient(f, x)
-        x -= lr * grad
+                grad = numerical_gradient(f, x)
+                x -= lr * grad
 
-    return x, np.array(x_history)
+        return x, np.array(x_history)
 
 
 def function_2(x):
-    return x[0]**2 + x[1]**2
+        return x[0]**2 + x[1]**2
+
 
 if __name__ == '__main__':
-        init_x = np.array([-3.0, 4.0])    
+        init_x = np.array([-3.0, 4.0])
 
         lr = 0.1
         step_num = 20
-        x, x_history = gradient_descent(function_2, init_x, 
+        x, x_history = gradient_descent(function_2, init_x,
                                         lr=lr, step_num=step_num)
 
-        plt.plot( [-5, 5], [0,0], '--b')
-        plt.plot( [0,0], [-5, 5], '--b')
-        plt.plot(x_history[:,0], x_history[:,1], 'o')
+        plt.plot([-5, 5], [0, 0], '--b')
+        plt.plot([0, 0], [-5, 5], '--b')
+        plt.plot(x_history[:, 0], x_history[:, 1], 'o')
 
         plt.xlim(-3.5, 3.5)
         plt.ylim(-4.5, 4.5)

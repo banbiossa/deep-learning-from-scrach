0ffd6587bfaf8db6278272e526b78eb9ad6b11e2
Shota Shimizu <s_shimizu@pkshatech.com>
2018-11-04 22:16:54 +0900

    lint

--- src/common/util.py
+++ src/common/util.py
@@ -59,7 +59,8 @@ def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
         out_h = (H + 2*pad - filter_h)//stride + 1
         out_w = (W + 2*pad - filter_w)//stride + 1
 
-        img = np.pad(input_data, [(0, 0), (0, 0), (pad, pad), (pad, pad)], 'constant')
+        img = np.pad(input_data,
+                     [(0, 0), (0, 0), (pad, pad), (pad, pad)], 'constant')
         col = np.zeros((N, C, filter_h, filter_w, out_h, out_w))
 
         for y in range(filter_h):

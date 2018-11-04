daa0384da09c79c9862bd7b6e4471251a3479d34
Shota Shimizu <s_shimizu@pkshatech.com>
2018-11-04 22:21:52 +0900

    lint

--- src/common/trainer.py
+++ src/common/trainer.py
@@ -25,9 +25,14 @@ class Trainer:
                 self.evaluate_sample_num_per_epoch = evaluate_sample_num_per_epoch
 
                 # optimzer
-                optimizer_class_dict = {'sgd': SGD, 'momentum': Momentum, 'nesterov': Nesterov,
-                                        'adagrad': AdaGrad, 'rmsprpo': RMSprop, 'adam': Adam}
-                self.optimizer = optimizer_class_dict[optimizer.lower()](**optimizer_param)
+                optimizer_class_dict = {'sgd': SGD,
+                                        'momentum': Momentum,
+                                        'nesterov': Nesterov,
+                                        'adagrad': AdaGrad,
+                                        'rmsprpo': RMSprop,
+                                        'adam': Adam}
+                self.optimizer = \
+                    optimizer_class_dict[optimizer.lower()](**optimizer_param)
 
                 self.train_size = x_train.shape[0]
                 self.iter_per_epoch = max(self.train_size / mini_batch_size, 1)
@@ -55,21 +60,30 @@ class Trainer:
                 if self.current_iter % self.iter_per_epoch == 0:
                         self.current_epoch += 1
 
-                        x_train_sample, t_train_sample = self.x_train, self.t_train
-                        x_test_sample, t_test_sample = self.x_test, self.t_test
+                        x_train_sample, t_train_sample = \
+                            self.x_train, self.t_train
+                        x_test_sample, t_test_sample = \
+                            self.x_test, self.t_test
+
                         if not self.evaluate_sample_num_per_epoch is None:
                                 t = self.evaluate_sample_num_per_epoch
-                                x_train_sample, t_train_sample = self.x_train[:t], self.t_train[:t]
-                                x_test_sample, t_test_sample = self.x_test[:t], self.t_test[:t]
+                                x_train_sample, t_train_sample = \
+                                    self.x_train[:t], self.t_train[:t]
+                                x_test_sample, t_test_sample = \
+                                    self.x_test[:t], self.t_test[:t]
 
-                        train_acc = self.network.accuracy(x_train_sample, t_train_sample)
-                        test_acc = self.network.accuracy(x_test_sample, t_test_sample)
+                        train_acc = self.network.accuracy(x_train_sample,
+                                                          t_train_sample)
+                        test_acc = self.network.accuracy(x_test_sample,
+                                                         t_test_sample)
                         self.train_acc_list.append(train_acc)
                         self.test_acc_list.append(test_acc)
 
                         if self.verbose:
-                                print("=== epoch:" + str(self.current_epoch) + ", train acc:" +
-                                      str(train_acc) + ", test acc:" + str(test_acc) + " ===")
+                                logger.info(f"==============================")
+                                logger.info(f"epoch: {self.current_epoch}")
+                                logger.info(f"train acc: {train_acc}")
+                                logger.info(f"test acc: {test_acc}")
                 self.current_iter += 1
 
         def train(self):
@@ -79,5 +93,6 @@ class Trainer:
                 test_acc = self.network.accuracy(self.x_test, self.t_test)
 
                 if self.verbose:
-                        print("=============== Final Test Accuracy ===============")
-                        print("test acc:" + str(test_acc))
+                        logger.info(f"==============================")
+                        logger.info(f"Final Test Accuracy")
+                        logger.info(f"test acc: {test_acc}")

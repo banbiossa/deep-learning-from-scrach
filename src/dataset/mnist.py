d161dc0065df22ff30228a6f0b049b3e816ba5d3
Shota Shimizu <s_shimizu@pkshatech.com>
2018-11-04 22:46:55 +0900

    lint

--- src/dataset/mnist.py
+++ src/dataset/mnist.py
@@ -123,14 +123,17 @@ def load_mnist(normalize=True, flatten=True, one_hot_label=False):
                         dataset[key] /= 255.0
 
         if one_hot_label:
-                dataset['train_label'] = _change_one_hot_label(dataset['train_label'])
-                dataset['test_label'] = _change_one_hot_label(dataset['test_label'])
+                dataset['train_label'] = \
+                    _change_one_hot_label(dataset['train_label'])
+                dataset['test_label'] = \
+                    _change_one_hot_label(dataset['test_label'])
 
         if not flatten:
                 for key in ('train_img', 'test_img'):
                         dataset[key] = dataset[key].reshape(-1, 1, 28, 28)
 
-        return (dataset['train_img'], dataset['train_label']), (dataset['test_img'], dataset['test_label'])
+        return (dataset['train_img'], dataset['train_label']),\
+               (dataset['test_img'], dataset['test_label'])
 
 
 if __name__ == '__main__':

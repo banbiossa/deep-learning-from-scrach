5bdd61d38267c40e2fc62afd8c13b546aaaa7f68
Shota Shimizu <s_shimizu@pkshatech.com>
2018-11-04 22:28:49 +0900

    lint

--- src/common/multi_layer_net.py
+++ src/common/multi_layer_net.py
@@ -24,7 +24,8 @@ class MultiLayerNet:
         """
 
         def __init__(self, input_size, hidden_size_list, output_size,
-                     activation='relu', weight_init_std='relu', weight_decay_lambda=0):
+                     activation='relu', weight_init_std='relu',
+                     weight_decay_lambda=0):
                 self.input_size = input_size
                 self.output_size = output_size
                 self.hidden_size_list = hidden_size_list
@@ -39,14 +40,16 @@ class MultiLayerNet:
                 activation_layer = {'sigmoid': Sigmoid, 'relu': Relu}
                 self.layers = OrderedDict()
                 for idx in range(1, self.hidden_layer_num+1):
-                        self.layers['Affine' + str(idx)] = Affine(self.params['W' + str(idx)],
-                                                                  self.params['b' + str(idx)])
-                        self.layers['Activation_function' +
-                                    str(idx)] = activation_layer[activation]()
+                        self.layers['Affine' + str(idx)] = \
+                            Affine(self.params['W' + str(idx)],
+                                   self.params['b' + str(idx)])
+                        self.layers['Activation_function' + str(idx)] =\
+                            activation_layer[activation]()
 
                 idx = self.hidden_layer_num + 1
-                self.layers['Affine' + str(idx)] = Affine(self.params['W' + str(idx)],
-                                                          self.params['b' + str(idx)])
+                self.layers['Affine' + str(idx)] = \
+                    Affine(self.params['W' + str(idx)],
+                           self.params['b' + str(idx)])
 
                 self.last_layer = SoftmaxWithLoss()
 
@@ -59,17 +62,21 @@ class MultiLayerNet:
                     'relu'または'he'を指定した場合は「Heの初期値」を設定
                     'sigmoid'または'xavier'を指定した場合は「Xavierの初期値」を設定
                 """
-                all_size_list = [self.input_size] + self.hidden_size_list + [self.output_size]
+                all_size_list = [self.input_size] + \
+                    self.hidden_size_list + [self.output_size]
                 for idx in range(1, len(all_size_list)):
                         scale = weight_init_std
                         if str(weight_init_std).lower() in ('relu', 'he'):
-                                scale = np.sqrt(2.0 / all_size_list[idx - 1])  # ReLUを使う場合に推奨される初期値
-                        elif str(weight_init_std).lower() in ('sigmoid', 'xavier'):
+                                scale = np.sqrt(2.0 / all_size_list[idx - 1])
+                                # ReLUを使う場合に推奨される初期値
+                        elif str(weight_init_std).lower() in ('sigmoid',
+                                                              'xavier'):
                                 # sigmoidを使う場合に推奨される初期値
                                 scale = np.sqrt(1.0 / all_size_list[idx - 1])
 
                         self.params['W' + str(idx)] = scale * \
-                            np.random.randn(all_size_list[idx-1], all_size_list[idx])
+                            np.random.randn(all_size_list[idx-1],
+                                            all_size_list[idx])
                         self.params['b' + str(idx)] = np.zeros(all_size_list[idx])
 
         def predict(self, x):
@@ -95,7 +102,8 @@ class MultiLayerNet:
                 weight_decay = 0
                 for idx in range(1, self.hidden_layer_num + 2):
                         W = self.params['W' + str(idx)]
-                        weight_decay += 0.5 * self.weight_decay_lambda * np.sum(W ** 2)
+                        weight_decay += \
+                            0.5 * self.weight_decay_lambda * np.sum(W ** 2)
 
                 return self.last_layer.forward(y, t) + weight_decay
 
@@ -126,10 +134,12 @@ class MultiLayerNet:
 
                 grads = {}
                 for idx in range(1, self.hidden_layer_num+2):
-                        grads['W' + str(idx)] = numerical_gradient(loss_W,
-                                                                   self.params['W' + str(idx)])
-                        grads['b' + str(idx)] = numerical_gradient(loss_W,
-                                                                   self.params['b' + str(idx)])
+                        grads['W' + str(idx)] = \
+                            numerical_gradient(loss_W,
+                                               self.params['W' + str(idx)])
+                        grads['b' + str(idx)] = \
+                            numerical_gradient(loss_W,
+                                               self.params['b' + str(idx)])
 
                 return grads
 
@@ -162,8 +172,11 @@ class MultiLayerNet:
                 # 設定
                 grads = {}
                 for idx in range(1, self.hidden_layer_num+2):
-                        grads['W' + str(idx)] = self.layers['Affine' + str(idx)].dW + \
-                            self.weight_decay_lambda * self.layers['Affine' + str(idx)].W
-                        grads['b' + str(idx)] = self.layers['Affine' + str(idx)].db
+                        grads['W' + str(idx)] = \
+                            self.layers['Affine' + str(idx)].dW + \
+                            self.weight_decay_lambda * \
+                            self.layers['Affine' + str(idx)].W
+                        grads['b' + str(idx)] = \
+                            self.layers['Affine' + str(idx)].db
 
                 return grads

5fedd31327f490697c70376d7f40747ce4b518f7
Shota Shimizu <s_shimizu@pkshatech.com>
2018-11-18 14:49:30 +0900

    finish optimizers

--- src/ch06/optimizer_compare_mnist.py
+++ src/ch06/optimizer_compare_mnist.py
@@ -1,15 +1,21 @@
 # coding: utf-8
 import matplotlib.pyplot as plt
-from dataset.mnist import load_mnist
+import numpy as np
+from ..dataset.mnist import load_mnist
 from ..common.util import smooth_curve
 from ..common.multi_layer_net import MultiLayerNet
 from ..common.optimizer import SGD, Momentum, Nesterov, AdaGrad, RMSprop
 from ..common.optimizer import Adam
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def main():
+        logger = logging.getLogger(__name__)
 
         # 0:MNISTデータの読み込み==========
+        logger.info("0:MNISTデータの読み込み==========")
         (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
 
         train_size = x_train.shape[0]
@@ -17,12 +23,14 @@ def main():
         max_iterations = 2000
 
         # 1:実験の設定==========
+        logger.info("1:実験の設定==========")
         optimizers = {}
         optimizers['SGD'] = SGD()
         optimizers['Momentum'] = Momentum()
         optimizers['AdaGrad'] = AdaGrad()
         optimizers['Adam'] = Adam()
         #optimizers['RMSprop'] = RMSprop()
+        logger.info(f"Optimizers: {optimizers}")
 
         networks = {}
         train_loss = {}
@@ -33,6 +41,7 @@ def main():
                 train_loss[key] = []
 
         # 2:訓練の開始==========
+        logger.info(f"2:訓練の開始===={max_iterations} iters ==== ")
         for i in range(max_iterations):
                 batch_mask = np.random.choice(train_size, batch_size)
                 x_batch = x_train[batch_mask]
@@ -46,12 +55,13 @@ def main():
                         train_loss[key].append(loss)
 
                 if i % 100 == 0:
-                        print("===========" + "iteration:" + str(i) + "===========")
+                        logger.info(f"==== iteration: {i} =====")
                         for key in optimizers.keys():
                                 loss = networks[key].loss(x_batch, t_batch)
                                 print(key + ":" + str(loss))
 
         # 3.グラフの描画==========
+        logger.info("3.グラフの描画==========")
         markers = {"SGD": "o", "Momentum": "x", "AdaGrad": "s", "Adam": "D"}
         x = np.arange(max_iterations)
         for key in optimizers.keys():
@@ -65,4 +75,5 @@ def main():
 
 
 if __name__ == "__main__":
+        logger.info("Kick main")
         main()
